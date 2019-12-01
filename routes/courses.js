717b95857c770a1af3d98016c15d36a9a72ebbc8
Nikolay Vokhlachev <nvokhlachev@gmail.com>
2019-12-01 23:01:54 +0200

    This project added

---
+++ routes/courses.js
@@ -0,0 +1,62 @@
+const {Router} = require('express')
+const Course =require('../models/course')
+const auth = require('../middleware/auth')
+const router = Router()
+
+router.get('/', async (req, res) => {
+    //const courses = await Course.getAll()
+    const courses = await Course.find()
+    .populate('userId', 'email name')
+    .select('price title img')
+
+    res.render('courses', {
+        title: 'COURSES',
+        isCourses: true,
+        courses
+    })
+})
+
+router.get('/:id/edit', auth, async (req, res) => {
+    if (!req.query.allow) {
+     return res.redirect('/')
+    }
+
+    //const course = await Course.getById(req.params.id)
+    const course = await Course.findById(req.params.id)
+
+    res.render('course-edit', {
+      title:`Edit ${course.title}`,
+      course 
+    })
+})
+
+router.post('/edit', auth, async (req, res) => {
+    //await Course.update(req.body)
+    const {id} = req.body
+    delete req.body.id
+    await Course.findByIdAndUpdate(id, req.body)
+    res.redirect('/courses')
+})
+
+router.post('/remove', auth, async (req, res) => {
+    try {
+        await Course.deleteOne({_id: req.body.id})
+        res.redirect('/courses')
+    }catch (e) {
+        console.log(e)
+    }
+})
+
+router.get('/:id', async (req, res) => {
+   //const course = await Course.getById(req.params.id)
+  // console.log('ID', req.params.id)
+    const course = await Course.findById(req.params.id)
+    res.render('course', {
+        layout: 'empty',
+        title: `Course ${course.title}`,
+        course
+    })
+})
+
+
+module.exports = router
